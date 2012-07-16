6506c6821dd5f8ce3c6130952a52420d90d705ca
GitSchmidt <uweschmidt.it@googlemail.com>
2012-07-16 23:39:00 +0200

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 DemoApp2::Application.routes.draw do
-  get "static_pages/home"
-
-  get "static_pages/help"
-
+	get "static_pages/home"
+	get "static_pages/help"
+	get "static_pages/about"
+  
   resources :microposts
 
   resources :users
